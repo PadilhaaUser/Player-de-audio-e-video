fbc0c88505704129291482ed5ffb0c00e0479928
Paulo Padilha <padilha.dev00@gmail.com>
2024-02-29 15:41:27 -0300

    Ultimo commit

---
+++ PlayerC/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PlayerC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstPlayList = new System.Windows.Forms.ListBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.ofdAbrir = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSalvar = new System.Windows.Forms.SaveFileDialog();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstPlayList
+            // 
+            this.lstPlayList.FormattingEnabled = true;
+            this.lstPlayList.ItemHeight = 15;
+            this.lstPlayList.Location = new System.Drawing.Point(12, 356);
+            this.lstPlayList.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.lstPlayList.Name = "lstPlayList";
+            this.lstPlayList.Size = new System.Drawing.Size(664, 139);
+            this.lstPlayList.TabIndex = 1;
+            this.lstPlayList.DoubleClick += new System.EventHandler(this.LstPlayList_DoubleClick);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnAbrir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAbrir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAbrir.Location = new System.Drawing.Point(14, 504);
+            this.btnAbrir.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(86, 40);
+            this.btnAbrir.TabIndex = 2;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.BtnAbrir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSalvar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalvar.Location = new System.Drawing.Point(134, 505);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(86, 39);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnCarregar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCarregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCarregar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCarregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCarregar.Location = new System.Drawing.Point(470, 504);
+            this.btnCarregar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(86, 39);
+            this.btnCarregar.TabIndex = 4;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = false;
+            this.btnCarregar.Click += new System.EventHandler(this.BtnCarregar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnLimpar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLimpar.Location = new System.Drawing.Point(590, 504);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(86, 39);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(12, 12);
+            this.player.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(664, 340);
+            this.player.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(691, 556);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.lstPlayList);
+            this.Controls.Add(this.player);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Player";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.ListBox lstPlayList;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.OpenFileDialog ofdAbrir;
+        private System.Windows.Forms.SaveFileDialog sfdSalvar;
+    }
+}
+
---
+++ PlayerC/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace PlayerC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        WMPLib.IWMPPlaylist playList;
+        
+        private void BtnAbrir_Click(object sender, EventArgs e)
+        {
+            ofdAbrir.Title = "Abrir mídia";
+            ofdAbrir.Filter = "Arquivo mp4|*.mp4|Arquvio mp3|*.mp3";
+           if (ofdAbrir.ShowDialog() == DialogResult.OK )
+            {
+                playList = player.playlistCollection.newPlaylist("Lista");
+
+                foreach (var arquivo in ofdAbrir.FileNames)
+                {
+                    playList.appendItem(player.newMedia(arquivo));
+                    lstPlayList.Items.Add(arquivo);
+
+
+                    player.currentPlaylist = playList;
+                    player.Ctlcontrols.play();
+                }
+            }
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            if(lstPlayList.Items.Count > 0)
+            {
+                sfdSalvar.Title = "Salvar PlayList";
+                sfdSalvar.Filter = "Arquivo texto|*.txt";
+                if(sfdSalvar.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter arquivo = new StreamWriter(sfdSalvar.FileName, false);
+                    for (int i = 0; i < lstPlayList.Items.Count ; i++)
+                    {
+                        arquivo.WriteLine(lstPlayList.Items[i].ToString());
+                    }
+                    arquivo.Close();
+                }
+                
+            }
+            
+
+        }
+
+        private void BtnCarregar_Click(object sender, EventArgs e)
+        {
+            ofdAbrir.Title = "Abrir PlayList";
+            ofdAbrir.Filter = "Arquivo texto|*.txt";
+            ofdAbrir.Multiselect = false;
+            if (ofdAbrir.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader arquivo = new StreamReader(ofdAbrir.FileName);
+                while (arquivo.Peek() != -1)
+                {
+                    lstPlayList.Items.Add(arquivo.ReadLine());
+                }
+                arquivo.Close();
+            }
+        }
+
+        private void LstPlayList_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstPlayList.Items.Count > 0)
+            {
+                player.URL = lstPlayList.SelectedItem.ToString();
+                player.Ctlcontrols.play();
+            }
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.stop();
+            lstPlayList.Items.Clear();
+        }
+    }
+}
